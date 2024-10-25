8393bbfdbe4b7ec5b7d239003ecc1bef58f820d2
jonathanreyesgarcia <jonathan.reyes@macaqueconsulting.com>
2024-10-25 23:08:58 +0100

    Actualizar DATABASE_URL para usar la URL externa de Render

--- app/database.py
+++ app/database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://tanamix22:PafBFaytY80Gm6ACraqOsZHlSIXdRo8C@dpg-cse0tum8ii6s73b19a20-a:5432/esmeraldadb"
+# Usar la URL externa proporcionada por Render
+DATABASE_URL = "postgresql://tanamix22:PafBFaytY80Gm6ACraqOsZHlSIXdRo8C@dpg-cse0tum8ii6s73b19a20-a.oregon-postgres.render.com:5432/esmeraldadb"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
