80f38929124720a52c0a1033bba7df1b646a4ca8
jonathanreyesgarcia <jonathan.reyes@macaqueconsulting.com>
2024-10-25 22:54:08 +0100

    Agregar script para crear tablas en la base de datos

--- app/main.py
+++ app/main.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI
 from .routers import blog
+from .database import Base, engine
+from . import models
+
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
