17a31763188a672f192f3fa0374d69d9f6a24ecc
jonathanreyesgarcia <jonathan.reyes@macaqueconsulting.com>
2024-11-25 10:09:47 +0000

    env

--- app/database.py
+++ app/database.py
@@ -1,9 +1,14 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 
-# Usar la URL externa proporcionada por Render
-DATABASE_URL = "postgresql://tanamix22:Agl6eavXkd1cAsXxHu7GmgjMRawHatvd@dpg-ct24gb0gph6c73bmr6kg-a.oregon-postgres.render.com/esmeraldadb_ypiv"
+# Cargar las variables de entorno desde el archivo .env
+load_dotenv()
+
+# Usar la URL de la base de datos desde la variable de entorno
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
