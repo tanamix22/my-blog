5fad10623f952ee2b2ae0cc61730f57e1233168d
jonathanreyesgarcia <jonathan.reyes@macaqueconsulting.com>
2024-11-25 09:56:07 +0000

    change pw

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # Usar la URL externa proporcionada por Render
-DATABASE_URL = "postgresql://tanamix22:PafBFaytY80Gm6ACraqOsZHlSIXdRo8C@dpg-cse0tum8ii6s73b19a20-a.oregon-postgres.render.com:5432/esmeraldadb"
+DATABASE_URL = "postgresql://tanamix22:Agl6eavXkd1cAsXxHu7GmgjMRawHatvd@dpg-ct24gb0gph6c73bmr6kg-a.oregon-postgres.render.com/esmeraldadb_ypiv"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
