33ab04bdfa5a5fd39a62c9b6c9c93939daa710fb
nahyoomi <nahyomi2000@gmail.com>
2024-11-29 10:43:31 +0000

    Added new endponit for blog

--- app/routers/blog.py
+++ app/routers/blog.py
@@ -1,19 +1,16 @@
 from fastapi import APIRouter, Depends, HTTPException, UploadFile, File
 from sqlalchemy.orm import Session
 from typing import List
-
 from ..crud import crud
 from ..auth.jwt_bearer import JWTBearer
-
 from ..models import models
 from ..schemas import schemas
 from ..database import SessionLocal, engine
-import shutil
-import os
+
 
 models.Base.metadata.create_all(bind=engine)
 
-router = APIRouter()
+router = APIRouter(tags=["blog"], prefix="/blogs")
 
 def get_db():
     db = SessionLocal()
@@ -22,31 +19,31 @@ def get_db():
     finally:
         db.close()
 
-@router.post("/blogs/", response_model=schemas.Blog, dependencies=[Depends(JWTBearer())])
+@router.post("/", response_model=schemas.Blog, dependencies=[Depends(JWTBearer())])
 def create_blog(
     blog: schemas.BlogCreate,
     db: Session = Depends(get_db)
 ):
     return crud.create_blog(db=db, blog=blog)
 
-@router.get("/blogs/", response_model=List[schemas.Blog])
+@router.get("/", response_model=List[schemas.Blog])
 def read_blogs(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
     blogs = crud.get_blogs(db, skip=skip, limit=limit)
     return blogs
 
-@router.get("/blogs/{blog_id}", response_model=schemas.Blog, dependencies=[Depends(JWTBearer())])
+@router.get("/{blog_id}", response_model=schemas.Blog, dependencies=[Depends(JWTBearer())])
 def read_blog(blog_id: int, db: Session = Depends(get_db)):
     db_blog = crud.get_blog(db, blog_id=blog_id)
     if db_blog is None:
         raise HTTPException(status_code=404, detail="Blog not found")
     return db_blog
 
-@router.delete("/blogs/{blog_id}", response_model=schemas.Blog, dependencies=[Depends(JWTBearer())])
+@router.delete("/{blog_id}", response_model=schemas.Blog, dependencies=[Depends(JWTBearer())])
 def delete_blog(blog_id: int, db: Session = Depends(get_db)):
     db_blog = crud.delete_blog(db, blog_id=blog_id)
     return db_blog
 
-@router.put("/blogs/{blog_id}", response_model=schemas.Blog, dependencies=[Depends(JWTBearer())])
+@router.put("/{blog_id}", response_model=schemas.Blog, dependencies=[Depends(JWTBearer())])
 def update_blog(
     blog_id: int,
     blog: schemas.BlogUpdate,
--- app/routers/homepage.py
+++ app/routers/homepage.py
@@ -4,7 +4,6 @@ from ..schemas import schemas
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from typing import List
-
 from ..crud import homepage_crud
 from ..models.homepage_models import HomePageImage
 from ..schemas.homepage_schemas import HomePageImage as HomePageImageSchema, HomePageImageCreate, HomePageImageUpdate
@@ -13,7 +12,7 @@ from ..auth.jwt_bearer import JWTBearer
 
 HomePageImage.metadata.create_all(bind=engine)
 
-router = APIRouter()
+router = APIRouter(tags=["Home Services"], prefix="/home")
 
 def get_db():
     db = SessionLocal()
@@ -22,31 +21,31 @@ def get_db():
     finally:
         db.close()
 
-@router.post("/homepage/images/", response_model=HomePageImageSchema, dependencies=[Depends(JWTBearer())])
+@router.post("/images", response_model=HomePageImageSchema, dependencies=[Depends(JWTBearer())])
 def create_image(
     image: HomePageImageCreate,
     db: Session = Depends(get_db)
 ):
     return homepage_crud.create_image(db=db, image=image)
 
-@router.get("/homepage/images/", response_model=List[HomePageImageSchema])
+@router.get("/images", response_model=List[HomePageImageSchema])
 def read_images(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
     images = homepage_crud.get_images(db, skip=skip, limit=limit)
     return images
 
-@router.get("/homepage/images/{image_id}", response_model=HomePageImageSchema, dependencies=[Depends(JWTBearer())])
+@router.get("/images/{image_id}", response_model=HomePageImageSchema, dependencies=[Depends(JWTBearer())])
 def read_image(image_id: int, db: Session = Depends(get_db)):
     db_image = homepage_crud.get_image(db, image_id=image_id)
     if db_image is None:
         raise HTTPException(status_code=404, detail="Image not found")
     return db_image
 
-@router.delete("/homepage/images/{image_id}", response_model=HomePageImageSchema, dependencies=[Depends(JWTBearer())])
+@router.delete("/images/{image_id}", response_model=HomePageImageSchema, dependencies=[Depends(JWTBearer())])
 def delete_image(image_id: int, db: Session = Depends(get_db)):
     db_image = homepage_crud.delete_image(db, image_id=image_id)
     return db_image
 
-@router.put("/homepage/images/{image_id}", response_model=HomePageImageSchema, dependencies=[Depends(JWTBearer())])
+@router.put("/images/{image_id}", response_model=HomePageImageSchema, dependencies=[Depends(JWTBearer())])
 def update_image(
     image_id: int,
     image: HomePageImageUpdate,
